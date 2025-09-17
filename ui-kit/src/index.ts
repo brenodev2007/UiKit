6608c9b130202880a4a031534beae43fc040e47d
Breno <breno.soriani@gmail.com>
2025-09-17 20:32:24 -0300

    Centralizando tudo no index.ts

---
+++ ui-kit/src/index.ts
@@ -0,0 +1,44 @@
+export * from "./components/button/Button";
+export * from "./components/button/ButtonIcon";
+
+export * from "./components/cards/Cards";
+
+export * from "./components/extras/Avatar";
+export * from "./components/extras/Loader";
+
+export * from "./components/feedbacks/Alert";
+export * from "./components/feedbacks/Badge";
+export * from "./components/feedbacks/Modal";
+export * from "./components/feedbacks/ModalForm";
+export * from "./components/feedbacks/Text";
+export * from "./components/feedbacks/Toast";
+export * from "./components/feedbacks/Tooltip";
+
+export * from "./components/input/CheckBox";
+export * from "./components/input/Radio";
+export * from "./components/input/RadioGroup";
+export * from "./components/input/SelectDropDown";
+export * from "./components/input/TextField";
+export * from "./components/input/Textarea";
+export * from "./components/input/Toggle";
+
+export * from "./components/list/PageTable";
+export * from "./components/list/SimpleList";
+export * from "./components/list/Table";
+
+export * from "./components/navbar/Breadcrumbs";
+export * from "./components/navbar/NavBar";
+export * from "./components/navbar/Pagination";
+export * from "./components/navbar/SideBar";
+export * from "./components/navbar/Tabs";
+
+export * from "./components/templates/form/Loginform";
+export * from "./components/templates/form/RecoveryForm";
+export * from "./components/templates/form/RegisterForm";
+
+export * from "./components/templates/layout/AboutSection";
+export * from "./components/templates/layout/FAQSection";
+export * from "./components/templates/layout/FooterSection";
+export * from "./components/templates/layout/HeroSection";
+export * from "./components/templates/layout/PricingSection";
+export * from "./components/templates/layout/Testmonial";
