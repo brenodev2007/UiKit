cb8c7992b72f7171564660169194bff0cb9cbded
Breno <breno.soriani@gmail.com>
2025-09-17 20:52:48 -0300

    publicando minha lib

--- ui-kit/vite.config.ts
+++ ui-kit/vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "UIKit",
+      fileName: (format) => `ui-kit.${format}.js`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    outDir: "dist",
+  },
 });
